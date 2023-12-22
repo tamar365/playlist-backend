d8b413c2299b9cb2c5b39ebaf4ee027644bcabfd
tamar365 <tamar.learningd@gmail.com>
2023-12-22 09:33:29 +0200

    when user register... login too

--- BL/userLogic.js
+++ BL/userLogic.js
@@ -2,49 +2,53 @@ const user = require("../DL/controllers/userController");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+async function createUser(input) {
+  const userAlreadyExist = await user.readOne(input.username);
+  if (userAlreadyExist) {
+    return { message: "user name already exists" };
+  }
+  const hashedPassword = await bcrypt.hash(input.password, 10);
+  const newUser = {
+    username: input.username,
+    password: hashedPassword,
+  };
+  const userFromDB = await user.create(newUser);
 
-async function createUser (input) {
-    const userAlreadyExist = await user.readOne(input.username);
-    if (userAlreadyExist) {
-        return ({message: "user name already exists"});
+  if (userFromDB) {
+    return await loginUser(input);
+  }
+}
+
+async function loginUser(userInput) {
+  if (userInput.username && userInput.password) {
+    let isValidLogin = false;
+    let existsUser = await user.readOne(userInput.username);
+
+    if (existsUser) {
+      isValidLogin = await bcrypt.compare(
+        userInput.password,
+        existsUser.password
+      );
     }
-    const hashedPassword = await bcrypt.hash(input.password, 10);
-    const newUser = {
-      username:input.username, password:hashedPassword
+
+    if (isValidLogin) {
+      const accessToken = jwt.sign(
+        JSON.stringify(existsUser),
+        process.env.TOKEN_SECRET
+      );
+      existsUser.password = undefined;
+      return accessToken;
+    } else {
+      throw { message: "Invalid credentials" };
     }
-    const userFromDB = await user.create(newUser)
-    return userFromDB.username;
-};
+  } else {
+    throw { message: "Status 400 => Please supply username and password" };
+  }
+}
 
-async function loginUser (userInput) {
-  
-    if (userInput.username && userInput.password) {
-        let isValidLogin = false;
-        let existsUser = await user.readOne(userInput.username);
-        
-        if (existsUser) {
-          isValidLogin = await bcrypt.compare(userInput.password, existsUser.password);
-        }
-         
-        if (isValidLogin) {
-          const accessToken = jwt.sign(
-            JSON.stringify(existsUser),
-            process.env.TOKEN_SECRET
-          );
-          existsUser.password = undefined;
-          return accessToken;
-        } else {
-          throw ({message: "Invalid credentials"})
-        }
-      } else {
-        throw ({message: "Status 400 => Please supply username and password"});
-      }
-};
-
-
-async function getUsers(){
+async function getUsers() {
   const users = await user.read();
   return users;
 }
 
-module.exports = {createUser, loginUser, getUsers};
+module.exports = { createUser, loginUser, getUsers };
